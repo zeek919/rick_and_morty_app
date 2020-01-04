96b08b26b4a88cac5e2a06d516e2648c254b4279
Zeek918 <zeek19988@wp.pl>
2020-01-04 02:15:09 +0100

    Added view of character page

---
+++ src/js/constants/characterSlider.js
@@ -0,0 +1,2 @@
+export const RADIO_SLIDER_BUTTON = document.querySelector('#slider');
+export const RADIO_MENU = document.querySelector('#radio-container');
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-unresolved */
 import '../scss/main.scss';
 import { HAMBURGER_BUTTON, NAVIGATION_MENU } from './constants/hamburger';
+import { RADIO_SLIDER_BUTTON, RADIO_MENU } from './constants/characterSlider';
 import { classToggler } from './utils/classToggler';
 
 classToggler(HAMBURGER_BUTTON, NAVIGATION_MENU, 'open', 'show');
+classToggler(RADIO_SLIDER_BUTTON, RADIO_MENU, 'arrow-slide', 'radio-menu-slide');
